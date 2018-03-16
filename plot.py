b5a9534a0edb04e6d510082bae711b2c27a51fb0
Dibyendu <dibyendumondal14@gmail.com>
2018-03-16 14:57:23 -0400

    added plots

---
+++ plot.py
@@ -0,0 +1,31 @@
+import pickle
+import matplotlib.pyplot as plt
+
+f = open("data4.pkl","r");
+
+rcll, prec, ap_desc_str = pickle.load(f)
+
+plt.step(rcll, prec, color='b', alpha=0.2, where='post')
+plt.fill_between(rcll, prec, step='post', alpha=0.2, color='b')
+plt.xlabel('Recall'); plt.ylabel('Precision')
+plt.ylim([0.0, 1.05]); plt.xlim([0.0, 1.0])
+plt.title(ap_desc_str)
+plt.show()
+
+rcll, prec, ap_desc_str = pickle.load(f)
+
+plt.step(rcll, prec, color='b', alpha=0.2, where='post')
+plt.fill_between(rcll, prec, step='post', alpha=0.2, color='b')
+plt.xlabel('Recall'); plt.ylabel('Precision')
+plt.ylim([0.0, 1.05]); plt.xlim([0.0, 1.0])
+plt.title(ap_desc_str)
+plt.show()
+
+rcll, prec, ap_desc_str = pickle.load(f)
+
+plt.step(rcll, prec, color='b', alpha=0.2, where='post')
+plt.fill_between(rcll, prec, step='post', alpha=0.2, color='b')
+plt.xlabel('Recall'); plt.ylabel('Precision')
+plt.ylim([0.0, 1.05]); plt.xlim([0.0, 1.0])
+plt.title(ap_desc_str)
+plt.show()
